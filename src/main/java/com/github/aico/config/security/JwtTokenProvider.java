218d57f104a91e3a0cc3b31555d3686676e37337
limhhyeon <honghyeon1007@gmail.com>
2025-02-22 14:09:04 +0900

    feat: add JwtTokenProvider && SecurityConfig add JwtTokenProvider apply

--- src/main/java/com/github/aico/config/security/JwtTokenProvider.java
+++ src/main/java/com/github/aico/config/security/JwtTokenProvider.java
@@ -1,15 +1,24 @@
 package com.github.aico.config.security;
 
+import com.github.aico.repository.userDetails.CustomUserDetails;
+import com.github.aico.service.exceptions.TokenValidateException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 
 
 @Slf4j
@@ -27,6 +36,18 @@ public class JwtTokenProvider {
     private final long tokenValidMilisecond = 2000L * 60 * 60; // 2시간
 
     private final UserDetailsService userDetailsService;
+    public String createToken(String email, List<String> roles){
+        Claims claims = Jwts.claims()
+                .setSubject(email);
+        claims.put("roles",roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime()+tokenValidMilisecond))
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .compact();
+    }
 
     public String resolveToken(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
@@ -38,9 +59,34 @@ public class JwtTokenProvider {
 
 
     public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            Date now = new Date();
+            if (claims.getExpiration().before(now)){
+                throw new TokenValidateException("해당 토큰 유효 기간이 지났습니다.");
+            }
+            return claims.getExpiration().after(now);
+        }catch (TokenValidateException tve){
+            throw new TokenValidateException("토큰이 유효하지 않습니다.");
+        }
 
     }
 
     public Authentication getAuthentication(String token) {
+        String email = getEmail(token);
+        UserDetails userDetails =userDetailsService.loadUserByUsername(email);
+        return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
+    }
+    public String getEmail(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 }
