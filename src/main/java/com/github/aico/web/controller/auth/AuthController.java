ffb7b636c51c0d318a426738dc1fa5112dfc25db
limhhyeon <honghyeon1007@gmail.com>
2025-02-24 13:16:27 +0900

    feat: add queryDsl dependencies && add queryDslConfig

--- src/main/java/com/github/aico/web/controller/auth/AuthController.java
+++ src/main/java/com/github/aico/web/controller/auth/AuthController.java
@@ -2,12 +2,15 @@ package com.github.aico.web.controller.auth;
 
 import com.github.aico.repository.user.JwtUser;
 import com.github.aico.repository.user.User;
+import com.github.aico.repository.user.UserRepository;
 import com.github.aico.repository.userDetails.CustomUserDetails;
 import com.github.aico.service.auth.AuthService;
+import com.github.aico.service.exceptions.NotFoundException;
 import com.github.aico.web.dto.auth.request.EmailDuplicate;
 import com.github.aico.web.dto.auth.request.LoginRequest;
 import com.github.aico.web.dto.auth.request.NicknameDuplicate;
 import com.github.aico.web.dto.auth.request.SignUpRequest;
+import com.github.aico.web.dto.auth.resposne.UserInfo;
 import com.github.aico.web.dto.base.ResponseDto;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class AuthController {
     private final AuthService authService;
+    private final UserRepository userRepository;
 
     @PostMapping("/nickname")
     public ResponseDto nickNameDuplicateCheck(@RequestBody NicknameDuplicate nicknameDuplicate){
@@ -52,7 +56,9 @@ public class AuthController {
         return authService.loginValidRequest(user);
     }
     @GetMapping("/test")
-    public String test(@JwtUser User user){
-        return user.getNickname();
+    public UserInfo test(@JwtUser User user){
+        log.info(user.getEmail());
+        User user1 = userRepository.findByEmailWithRoles(user.getEmail()).orElseThrow(()-> new NotFoundException("유저 찾기 불가"));
+        return UserInfo.from(user1);
     }
 }
