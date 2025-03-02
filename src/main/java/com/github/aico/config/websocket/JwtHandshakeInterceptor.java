e20132e036eadfd0b2c08bdfbc597bda0918cfa9
limhhyeon <honghyeon1007@gmail.com>
2025-03-02 09:55:01 +0900

    feat: add JwtHandshakeInterceptor

--- src/main/java/com/github/aico/config/websocket/JwtHandshakeInterceptor.java
+++ src/main/java/com/github/aico/config/websocket/JwtHandshakeInterceptor.java
@@ -1,56 +1,56 @@
-//package com.github.aico.config.websocket;
-//
-//import com.github.aico.config.security.JwtTokenProvider;
-//import jakarta.servlet.http.Cookie;
-//import jakarta.servlet.http.HttpServletRequest;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.server.ServerHttpRequest;
-//import org.springframework.http.server.ServerHttpResponse;
-//import org.springframework.http.server.ServletServerHttpRequest;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.socket.WebSocketHandler;
-//import org.springframework.web.socket.server.HandshakeInterceptor;
-//
-//import java.util.Map;
-//@RequiredArgsConstructor
-//@Slf4j
-//@Component
-//public class JwtHandshakeInterceptor implements HandshakeInterceptor {
-//    private final JwtTokenProvider jwtTokenProvider;
-//    @Override
-//    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-//        log.info("들어오려고 하니?");
-//        if (request instanceof ServletServerHttpRequest servletRequest) {
-//            HttpServletRequest httpServletRequest = servletRequest.getServletRequest();
-//            Cookie[] cookies = httpServletRequest.getCookies();
-//            if (cookies != null) {
-//                for (Cookie cookie : cookies) {
-//                    if ("access_token".equals(cookie.getName())) {
-//                        String token = cookie.getValue();
-//                       log.info("쿠키에 토큰 값: " + token);
-//
-//                        // 토큰 검증 (로그인한 유저인지 확인)
-//                        if (jwtTokenProvider.validateToken(token)) {
-//                            String userEmail = jwtTokenProvider.getEmail(token);
-//                            attributes.put("email", userEmail);
-//                            log.info("email: " + userEmail);
-//                            return true;
-//                        } else {
-//                            log.info("토큰이 유효하지 않습니다.");
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        response.setStatusCode(HttpStatus.UNAUTHORIZED);
-//        return false;
-//    }
-//
-//    @Override
-//    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
-//
-//    }
-//}
+package com.github.aico.config.websocket;
+
+import com.github.aico.config.security.JwtTokenProvider;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class JwtHandshakeInterceptor implements HandshakeInterceptor {
+    private final JwtTokenProvider jwtTokenProvider;
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        log.info("들어오려고 하니?");
+        if (request instanceof ServletServerHttpRequest servletRequest) {
+            HttpServletRequest httpServletRequest = servletRequest.getServletRequest();
+            Cookie[] cookies = httpServletRequest.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("access_token".equals(cookie.getName())) {
+                        String token = cookie.getValue();
+                       log.info("쿠키에 토큰 값: " + token);
+
+                        // 토큰 검증 (로그인한 유저인지 확인)
+                        if (jwtTokenProvider.validateToken(token)) {
+                            String userEmail = jwtTokenProvider.getEmail(token);
+                            attributes.put("email", userEmail);
+                            log.info("email: " + userEmail);
+                            return true;
+                        } else {
+                            log.info("토큰이 유효하지 않습니다.");
+                        }
+                    }
+                }
+            }
+        }
+
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
