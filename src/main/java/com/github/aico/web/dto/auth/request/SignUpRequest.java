34d849435a5a7f24547e853b001f9a41c32166f1
limhhyeon <honghyeon1007@gmail.com>
2025-03-05 10:48:11 +0900

    feat: password 정규식 검증 추가

--- src/main/java/com/github/aico/web/dto/auth/request/SignUpRequest.java
+++ src/main/java/com/github/aico/web/dto/auth/request/SignUpRequest.java
@@ -2,6 +2,7 @@ package com.github.aico.web.dto.auth.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +17,11 @@ public class SignUpRequest {
     @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "유효한 이메일 형식이 아닙니다.")
     private final String email;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+            message = "비밀번호는 8~20자이며, 소문자, 숫자, 특수문자(@$!%*?&)를 각각 1개 이상 포함해야 합니다."
+    )
     private final String password;
     private final String phoneNumber;
 
