74d41341193283a0c264ff310da87365cb3b2101
limhhyeon <honghyeon1007@gmail.com>
2025-03-09 16:50:44 +0900

    feat: 주석 처리

--- src/main/java/com/github/aico/config/websocket/WebsocketConfig.java
+++ src/main/java/com/github/aico/config/websocket/WebsocketConfig.java
@@ -22,7 +22,7 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/aiCoWebsocket")
                 .setAllowedOrigins("*");
-//                .addInterceptors(jwtHandshakeInterceptor);
+//                .addInterceptors(jwtHandshakeInterceptor);/
 
     }
 }
